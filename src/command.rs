e3ddec1b0e2e5130a786e8f4b681f6beae1a4292
Pietro Tamilia <17928339+BRA1L0R@users.noreply.github.com>
2024-11-02 13:43:01 +0100

    messages

---
+++ src/command.rs
@@ -0,0 +1,167 @@
+use serde::{de::Visitor, ser::SerializeTuple, Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct UnknownCommand {
+    command: u8,
+
+    /// guarantees that a user cannot build and send
+    /// an unknown command type
+    _unbuildable: (),
+}
+
+impl UnknownCommand {
+    fn new(command: u8) -> Self {
+        Self {
+            command,
+            _unbuildable: (),
+        }
+    }
+}
+
+macro_rules! impl_message {
+    ($($idx:expr => $def:ident { $($field:ident: $type:ty),+ }),+) => {
+        // #[repr(u8)]
+        #[derive(Debug)]
+        pub enum Message {
+            Unknown(UnknownCommand),
+            $($def { $($field: $type),+ } ),+
+        }
+
+        pub struct MessageEnumVisitor;
+        impl<'de> Visitor<'de> for MessageEnumVisitor {
+            type Value = Message;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("expected a seq of items")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let command: u8 = seq.next_element()?.unwrap();
+
+                match command {
+                    $(
+                        $idx => {
+                            #[derive(Deserialize)]
+                            struct Helper {
+                                $(
+                                    $field: $type,
+                                )+
+                            }
+
+                            let Helper { $($field),+ } = seq.next_element()?.unwrap();
+                            Ok(Message::$def { $($field),+ })
+                        }
+                    ),+
+                    _ => Ok(Message::Unknown(UnknownCommand::new(command))),
+                }
+            }
+        }
+
+        impl<'de> Deserialize<'de> for Message {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                deserializer.deserialize_tuple(2, MessageEnumVisitor)
+            }
+        }
+
+        impl Serialize for Message {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                let idx = match self {
+                    Self::Unknown(_) => unreachable!(),
+                    $(
+                        Self::$def { .. } => $idx ,
+                    )+
+                };
+
+                let mut tuple = serializer.serialize_tuple(2)?;
+                tuple.serialize_element(&idx)?;
+
+                match self {
+                    Self::Unknown(_) => unreachable!(),
+                    $(
+                        Self::$def { $($field),+ } => {
+                            #[derive(Serialize)]
+                            struct Helper<'a> {
+                                $($field: &'a $type),+
+                            }
+
+                            let helper = Helper { $($field),+ };
+                            tuple.serialize_element(&helper)?;
+                        },
+                    )+
+                };
+
+                tuple.end()
+            }
+        }
+    };
+}
+
+impl_message! {
+    b'j' => WheelSpeed { left: f32, right: f32 },
+    b'l' => LineSensor { left_line: u16, center_line: u16, right_line: u16 },
+    b'c' => ColorSensor { red: u16, green: u16, blue: u16 },
+    b'i' => Imu { ax: f32, ay: f32, az: f32, gx: f32, gy: f32, gz: f32 },
+    b'p' => Battery { value: f32 },
+    b'd' => Distance { left_tof: u16, center_tof: u16, right_tof: u16 },
+    b't' => Touch { value: u8 },
+    b'n' => TiltShake { value: u8 },
+    b'b' => Behaviour { value: u8 },
+    b'f' => DistanceMatrix { matrix: [u8; 7] },
+    b'q' => ImuPosition { roll: f32, pitch: f32, yaw: f32 },
+    b'w' => WheelsPosition { left_wheel: f32, right_wheel: f32 },
+    b'v' => Velocity { linear: f32, angular: f32 },
+    b'x' => AckU8 { ack: u8 },
+    b'z' => AckF32 { x: f32, y: f32, theta: f32 },
+    0x7E => FirmwareVersion { value: [u8; 3] }
+
+}
+
+// pub struct StructVisitor<'de, T: Deserialize<'de>>(PhantomData<T>);
+
+// impl<'de, T: Deserialize<'de>> Visitor<'de> for StructVisitor<'de, T> {
+//     fn visit
+// }
+
+// impl Serialize for Message {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//         let idx = match self {
+//             Self::Test(_) => b'A',
+//         };
+
+//         let mut tuple = serializer.serialize_tuple(2)?;
+
+//         tuple.serialize_element(&idx);
+//         match self {
+//             Self::Test(inner) => tuple.serialize_element(inner)?,
+//         };
+
+//         tuple.end()
+//     }
+// }
+
+// #[derive(Serialize, Deserialize, Debug)]
+// #[repr(u8)]
+// pub enum Message {
+//     B(u16, u16) = b'B',
+//     Tilda(u16) = b'~',
+// }
+
+// impl Serialize for Message {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: serde::Serializer,
+//     {
+//     }
+// }
---
+++ src/main.rs
@@ -0,0 +1,221 @@
+pub mod command;
+
+use std::{convert::Infallible, sync::Arc, thread::JoinHandle, time::Duration};
+
+use command::Message;
+use embassy_sync::{
+    blocking_mutex::raw::NoopRawMutex,
+    channel::{Channel, Sender},
+};
+use esp_idf_svc::{
+    hal::{
+        gpio::{AnyIOPin, IOPin, InputOutput, InputPin, OutputPin, PinDriver},
+        peripheral::Peripheral,
+        prelude::Peripherals,
+        task::{block_on, embassy_sync::EspRawMutex},
+        uart::{AsyncUartDriver, AsyncUartRxDriver, Uart, UartConfig, UartDriver, UartRxDriver},
+    },
+    sys::EspError,
+};
+use futures::future::join;
+use thiserror::Error;
+use ucpack::{is_complete_message, UcPack};
+
+#[derive(Error, Debug)]
+enum AlvikError {
+    #[error("alvik is turned off")]
+    Offline,
+    #[error("esp error: {0}")]
+    Esp(#[from] EspError),
+}
+
+struct AlvikSerial {
+    handle: JoinHandle<()>,
+    send_channel: AlvikChannel,
+    recv_channel: AlvikChannel,
+}
+
+type AlvikChannel = Arc<Channel<EspRawMutex, Message, 50>>;
+
+async fn alvik_task(
+    uart: UartDriver<'static>,
+    send_channel: AlvikChannel,
+    receive_channel: AlvikChannel,
+) -> Result<Infallible, AlvikError> {
+    let mut uart = AsyncUartDriver::wrap(uart)?;
+    uart.driver().clear_rx()?;
+
+    let (uart_tx, uart_rx) = uart.split();
+
+    let mut receive_buffer = Box::new([0u8; 512]);
+    let mut send_buffer = Box::new([0u8; 512]);
+
+    const PACK: UcPack = UcPack::new(b'A', b'#');
+
+    let receive_task = async move {
+        let mut cursor = 0;
+        while let Ok(read) = uart_rx.read(&mut receive_buffer[cursor..]).await {
+            // log::info!("read {read} bytes");
+            cursor += read;
+
+            let mut partial = 0;
+            while let Some(complete) = is_complete_message(&receive_buffer[partial..cursor]) {
+                let message: Message = match PACK.deserialize_slice(complete) {
+                    Ok(msg) => msg,
+                    Err(err) => {
+                        log::error!("error {err}");
+                        log::error!("{complete:x?}");
+                        panic!()
+                    }
+                };
+
+                receive_channel.send(message).await;
+                partial += complete.len();
+            }
+
+            receive_buffer.copy_within(partial.., 0);
+            cursor -= partial;
+        }
+    };
+
+    let send_task = async move {
+        // while let a  = send_channel.receive().await {};
+        loop {
+            let message = send_channel.receive().await;
+            let serialized = PACK
+                .serialize_slice(&message, &mut send_buffer[..])
+                .unwrap();
+
+            uart_tx.write(&send_buffer[..serialized]).await.unwrap();
+        }
+    };
+
+    // start the receive and send subroutines
+    join(receive_task, send_task).await;
+
+    panic!("left read loop");
+}
+
+impl AlvikSerial {
+    pub fn spawn(uart: UartDriver<'static>) -> Self {
+        let send_channel = Arc::new(Channel::<EspRawMutex, Message, 50>::new());
+        let recv_channel = Arc::new(Channel::<EspRawMutex, Message, 50>::new());
+
+        let handle = {
+            let send_channel = send_channel.clone();
+            let recv_channel = recv_channel.clone();
+            std::thread::spawn(move || {
+                let Err(err) = block_on(alvik_task(uart, send_channel, recv_channel));
+                panic!("alvik receiver task returned with error: {err}");
+            })
+        };
+
+        Self {
+            handle,
+            send_channel,
+            recv_channel,
+        }
+    }
+}
+
+struct AlvikDriver {
+    pub nrst: PinDriver<'static, AnyIOPin, InputOutput>,
+    pub check: PinDriver<'static, AnyIOPin, InputOutput>,
+    serial: AlvikSerial,
+}
+
+impl AlvikDriver {
+    fn init(
+        AlvikInterface {
+            mut nrst,
+            mut check,
+            mut uart,
+        }: AlvikInterface,
+    ) -> Result<Self, AlvikError> {
+        use esp_idf_svc::hal::gpio::Pull;
+        check.set_pull(Pull::Down).unwrap();
+
+        if check.is_low() {
+            return Err(AlvikError::Offline);
+        };
+
+        // reset alvik
+        nrst.set_low().unwrap();
+        std::thread::sleep(Duration::from_millis(200));
+        nrst.set_high().unwrap();
+        std::thread::sleep(Duration::from_millis(200));
+
+        let serial = AlvikSerial::spawn(uart);
+
+        block_on(async move {
+            let mut received = 0;
+            loop {
+                let msg = serial.recv_channel.receive().await;
+                received += 1;
+
+                if received % 20 == 0 {
+                    log::info!("{msg:?}");
+                }
+            }
+        });
+
+        todo!();
+    }
+}
+
+struct AlvikInterface {
+    pub nrst: PinDriver<'static, AnyIOPin, InputOutput>,
+    pub check: PinDriver<'static, AnyIOPin, InputOutput>,
+    pub uart: UartDriver<'static>,
+}
+
+impl AlvikInterface {
+    pub fn from_pins(
+        nrst: impl IOPin,
+        check: impl IOPin,
+        uart: impl Peripheral<P = impl Uart> + 'static,
+        tx: impl OutputPin,
+        rx: impl InputPin,
+    ) -> Result<Self, EspError> {
+        let (nrst, check) = (
+            PinDriver::input_output(nrst.downgrade())?,
+            PinDriver::input_output(check.downgrade())?,
+        );
+
+        let config = UartConfig::new().baudrate(460800.into());
+        let uart = UartDriver::new(
+            uart,
+            tx,
+            rx,
+            Option::<AnyIOPin>::None,
+            Option::<AnyIOPin>::None,
+            &config,
+        )?;
+
+        Ok(Self { nrst, check, uart })
+    }
+}
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+    log::info!("Starting the Rust alvik!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let interface = AlvikInterface::from_pins(
+        peripherals.pins.gpio6,
+        peripherals.pins.gpio13,
+        peripherals.uart1,
+        peripherals.pins.gpio43,
+        peripherals.pins.gpio44,
+    )
+    .unwrap();
+    let driver = AlvikDriver::init(interface).unwrap();
+
+    log::info!("Driver started!");
+}
